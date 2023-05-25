85919da45dc520c5388c2eec9b7547c117637b95
hn-mitch <michelle.apacible@au.harveynorman.com>
2023-05-26 02:35:12 +0800

    Refactor swiper js config

--- assets/slider.js
+++ assets/slider.js
@@ -1,22 +1,17 @@
-class SliderProductImages extends HTMLElement {
-  constructor() {
-    const config = {
+window.addEventListener('load', function() {
+    var swiper = new Swiper('.swiper', {
       slidesPerView: 3,
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev'
-            },
-            on: {
-              click: function(e) {
-                console.log(e.clickedIndex);
-                const imgIndex = e.clickedIndex;
-                const imgSrc = document.querySelector('.media-' + imgIndex + ' > img').getAttribute('src');
-                document.querySelector('.featured-image').src = imgSrc;
-              }
-            }
-    };
-    super(config);
-  }
-}
-
-customElements.define("slider-product-images", SliderProductImages);
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      on: {
+        click: function(e) {
+          console.log(e.clickedIndex);
+          const imgIndex = e.clickedIndex;
+          const imgSrc = document.querySelector('.media-' + imgIndex + ' > img').getAttribute('src');
+          document.querySelector('.featured-image').src = imgSrc;
+        }
+      }
+    });
+  });
